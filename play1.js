cdc661f1fcf808fbfe8c2dcc957fd69102e1c753
irina-27011987 <irinashevchenko27011987@gmail.com>
2026-03-22 20:44:59 +0300

    к карточке привязана игра + задания 5

---
+++ play1.js
@@ -0,0 +1,20 @@
+const playLink = document.querySelector('.play');
+playLink.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  startGame();
+});
+function startGame() {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    while (true) {
+        let guessNumber = prompt("Угадай число от 1 до 100");
+        guessNumber = Number(guessNumber);
+        if (guessNumber === randomNumber) {
+            alert("Поздравляю, ты угадал число!");
+            break;
+        } else if (guessNumber > randomNumber) {
+            alert("Твое число больше загаданного, попробуй еще!");
+        } else {
+            alert("Твое число меньше загаданного, попробуй еще!");
+        }
+    }
+}
